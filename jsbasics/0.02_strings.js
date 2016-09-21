62a2128852bc65e9f93c582b8bff28ef210a8ae3
Harriet Ross <Harriet Ross>
2016-09-21 11:20:27 -0400

    integer exercises

---
+++ jsbasics/0.02_strings.js
@@ -0,0 +1,32 @@
+var birthCity = "I was born in Los Angeles";
+console.log(birthCity);
+
+var username = "jamespauloconnor";
+var	password = "123LetMeIn!";
+var tweet =	"Kanye is broke again";
+var facebookPost = "I love coding!";
+var instaCaption = "#Awesome";
+var birthState = "California";
+var comma = ", ";
+var period = ".";
+
+console.log(username);
+console.log("                  ")
+console.log("Hello World");
+
+
+//concatenation
+console.log(birthCity + comma +  birthState);
+
+var graduationYear = " in 1994";
+var highSchool = "Carmel High School";
+
+console.log("I graduated from " + highSchool + graduationYear + period);
+
+var make = "Honda ";
+var model = "Odyssey ";
+var year = "2001";
+var makeModelYear = make + model + year;
+
+console.log(makeModelYear);
+
