8361d3894833cb83ab7c4452bfe27742be461345
Harriet Ross <Harriet Ross>
2016-09-21 10:33:46 -0400

    Added JS files and store app

---
+++ jsbasics/0.01_integers.js
@@ -0,0 +1,65 @@
+var num = 7;
+var newNum = 5; //this is camel casing
+var newer_num = 10; //this is snake casing
+
+
+//console.log() prints to the console
+
+//operations with console.log
+console.log (5+5);
+//TODO - MAKE A SUBTRACTION, MULTIPLICATION AND DIVISION CONSOLE.LOG()
+// console.log (10-5);
+// console.log (7*5);
+// console.log (200/40);
+
+//Modulus Operator gives remainder
+
+// console.log (10%5);
+// console.log (4%3);
+
+//TODO - make a modulus that returns zero and another that returns a remainder
+
+// console.log (12%6);
+// console.log (11%5);
+
+//TODO - try to make a variable that is assigned an operation and console log it 
+//example: var num =1 + 1
+
+// var num = (5*5);
+// console.log (num);
+
+// var a = 251;
+// var b = 361;
+// console.log (a*b);
+
+// var blanketBeach = 52;
+// var herName = 74;
+// var summer = blanketBeach*herName;
+
+var newNum = 10;
+newNum = newNum + 1;
+console.log(newNum);
+
+ //newNum++ = newNum + 1;
+ console.log(newNum);
+
+//newNum--newNum - 1;
+
+newNum = newNum  + 10;
+console.log(newNum);
+
+newNum+= 10;  //shortcut for writin newNum =newNum + 10;
+console.log(newNum);
+
+
+newNum -= 10;
+console.log(newNum);
+
+newNum -= 11;
+console.log(newNum);
+
+
+
+
+
+
